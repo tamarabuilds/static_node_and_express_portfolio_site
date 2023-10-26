db1edda4c96bd8b364afb7f9eab210daefbffb7d
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-10-26 15:48:40 -0700

    refactored error handling

--- app.js
+++ app.js
@@ -46,7 +46,6 @@ app.use((req, res, next) => {
  * 
  */
 app.use((err, req, res, next) => {
-    
     // confirm there's an error
     if (err){
         console.log(`Global error handler called`)
